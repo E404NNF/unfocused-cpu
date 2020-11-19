dafcfdacad3c585df20aa3e06a559e7edbd48e7c
E404NNF <37207067+E404NNF@users.noreply.github.com>
2020-11-19 04:21:08 +0200

    yes

---
+++ src/main/java/me/e404nnf/mods/unfocusedcpu/Main.java
@@ -0,0 +1,12 @@
+package me.e404nnf.mods.unfocusedcpu;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+		// sex
+	}
+
+}
---
+++ src/main/java/me/e404nnf/mods/unfocusedcpu/d/DoTheStuff.java
@@ -0,0 +1,21 @@
+package me.e404nnf.mods.unfocusedcpu.d;
+
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.GameRenderer;
+
+@Mixin(GameRenderer.class)
+public class DoTheStuff {
+	@Shadow @Final private MinecraftClient client;
+	
+	@Inject(method = "render", at = @At("HEAD"), cancellable = true)
+    private void onRenderHead(float tickDelta, long startTime, boolean tick, CallbackInfo info) {
+        if (!client.isWindowFocused()) info.cancel();
+    }
+}
